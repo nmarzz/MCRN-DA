6d3daf8e9fc7fd6e9738cf3f0c13f290f10b5b76
Rose Crocker <rose.crocker@adelaide.edu.au>
2020-01-22 14:51:16 +1030

    Added some lines for DMD projection, not fully working yet but can work on it tomorrow

---
+++ ParticleFilters/getDMD.m
@@ -0,0 +1,9 @@
+function [ q ] = getDMD( Phi,p )
+%Phi is the matrix of DMD modes
+[~,r] = size(Phi);
+if p<=r
+    q = Phi(:,1:p); %this get us p colmuns
+else
+    q = Phi; % this is when p>r
+end
+
