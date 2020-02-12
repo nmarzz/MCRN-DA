c339a90750930089a00a8cba37c6e817b8c61c63
albaraaa <38504804+albaraaa@users.noreply.github.com>
2020-02-12 17:46:09 -0500

    group_work

---
+++ Projected_Particle_Filters/New_Files/new_Init.m
@@ -0,0 +1,8 @@
+function [M,H,PinvH] = new_Init(N,inth,q)
+%Linear Observation operator, every inth variable
+Heye=eye(N,N);
+H=Heye(1:inth:end,:)*q;
+%M = Dimension of observation space.
+[M,~]=size(H)
+%yvars=linspace(1,M,M);
+PinvH=pinv(H);
---
+++ Projected_Particle_Filters/New_Files/projectionToggle_Physical.m
@@ -0,0 +1,10 @@
+function [q_physical] = projectionToggle_Physical(PhysicalModelProjection,Model_Dimension,Ur,p,Phi)
+% Type of projection for the Physical Model
+if PhysicalModelProjection == 0
+    q_physical = eye(Model_Dimension);
+elseif PhysicalModelProjection == 1
+    q_physical = getpod(Ur,p);
+elseif PhysicalModelProjection == 2
+    q_physical = getDMD( Phi,p );
+end
+
