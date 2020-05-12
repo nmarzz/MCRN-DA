08506d4717d5e410de84b1f12cb75ee7f92a351f
Aishah Albarakati <albaraaa@clarkson.edu>
2020-05-12 05:56:29 -0400

    fix

--- Projected_Particle_Filters/Physical_proj/projectionToggle_data.m
+++ Projected_Particle_Filters/Physical_proj/projectionToggle_data.m
@@ -5,14 +5,10 @@ function [U] = projectionToggle_data(DataModelProjection,Model_Dimension,Ur,p)
 % p: rank of projection
 if DataModelProjection == 0
     U = eye(Model_Dimension); %identity matrix
-%     q_data = eye(Model_Dimension); %identity matrix
 elseif DataModelProjection == 1
     U = getpod(Ur,p);
 elseif DataModelProjection == 2
     U = getDMD( Ur,p );
-%     q_data = getDMD( Ur,p );
     % elseif DataModelProjection == 3
     %    [U,~] = getausproj(Model_Dimension,p,Fmod,t,est,h,q,LE);
 end
-
-
