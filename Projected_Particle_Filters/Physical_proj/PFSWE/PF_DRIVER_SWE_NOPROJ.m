4cad028131f31886247f1410b7b77e636e9c17e2
Aishah Albarakati <albaraaa@clarkson.edu>
2020-05-28 19:49:06 -0400

    SWE_No proj is working and PFdrivSWE works with POD

--- Projected_Particle_Filters/Physical_proj/PFSWE/PF_DRIVER_SWE_NOPROJ.m
+++ Projected_Particle_Filters/Physical_proj/PFSWE/PF_DRIVER_SWE_NOPROJ.m
@@ -1,6 +1,17 @@
 %% Initialization
 clear all;clc;
 rng(1331);
+
+% Lorenz preamble
+% F = @FLor95; %Physical model
+% N =66; % N:Original model dimension
+%
+% % Build Model (via ODE45)
+% dt=1.E-2; % Model output time step
+% ModelSteps = 1500; % Number of time steps in building model
+% T=ModelSteps*dt;
+% Built_Model= buildModel(N,F,ModelSteps,T);
+
 % SWE preamble
 load('swerun.mat');
 F = @(t,x) formod(t,x,dt,pars);
@@ -10,12 +21,12 @@ IC = x_ics;
 
 %% Type of particle filter
 % Use of standard PF or OP-PF (iOPPF=0 => standard PF, iOPPF=1 => OP-PF)
-iOPPF=0;
+iOPPF=1;
 
 %% Projection_type(0 = no projection, 1 POD, 2 DMD, 3 AUS)
-PhysicalProjection = 1;
+PhysicalProjection = 0;
 DataProjection = 0;
-tolerance_physical = 20; % POD_modes
+tolerance_physical = 9; % POD_modes
 tolerance_data = 10; % POD_modes
 numModes_physical = 30;% DMD_modes, for physical
 numModes_data = 30; % DMD_modes, for data
@@ -34,7 +45,7 @@ alpha = 0;%alpha value for projected resampling
 ObsMult=1; % Observe and every ObsMult steps
 h = dt/ObsMult;
 Numsteps = size(Built_Model,2)*ObsMult;
-% Numsteps = Numsteps/2;
+Numsteps = Numsteps/2;
 %Observation Variance
 epsR = 0.01;
 %Model Variance
@@ -71,7 +82,6 @@ y = y + normrnd(0,R,M,size(Built_Model,2));
 [U] =projectionToggle_data(DataProjection,Ur_data,p_data);
 Rfixed = R;
 %%
-
 %Initial time and time step
 t=0;
 t0=t;
@@ -79,10 +89,7 @@ Resamps=0;
 RMSEave_orig=0;
 RMSEave_proj=0;
 iRMSE=1;
-
-Q=V'*Q*V; %with projection
-Q1=Q;
-
+Q1=Q*ones(1,N); %no Projection
 x=V'*u;
 %p_physical = 10;
 for i=1:Numsteps
@@ -93,23 +100,34 @@ for i=1:Numsteps
     [U] = projectionToggle_data(DataProjection,Ur_data,p_data);
     % Update noise covariance
     UPinvH = U(:,1:inth:end)'; % Multiplying by PinvH is equivalent to removing every inth row when H = H(1:inth:end,:)
+    % UPinvH = U' * PinvH = U' * pinv(H);
     R = UPinvH * Rfixed * UPinvH';
     Rinv = 1/R;
     
     if mod(i,ObsMult)==0
         %At observation times, Update weights via likelihood, add noise
-        HV=Hx(V,inth);  % with our assumptions left multipling
         if (iOPPF==0) % Standard Particle Filter
             x = x + mvnrnd(pzeros_physical,Q1,L)';
-            Innov=repmat(UPinvH*y(:,i),1,L)-HV*x;
+            Hnq=Hx(V,inth); % with our assumptions left multipling
+            Innov=repmat(UPinvH*y(:,i),1,L)-x(1:inth:end,:);
         else % IOPPF ==1, Optimal proposal PF
-            Innov=repmat(UPinvH*y(:,i),1,L)-HV*x;%proj
-            Qpinv = (1/alph)*eye(p_physical)+(1/bet)*(HV)'*(HV);
+            Hnq=Hx(V,inth);         
+            Innov=repmat(y(:,i),1,L)-Hnq*x(1:inth:end,:);
+            Qpinv = (1/alph)+(1/bet)*(Hnq*V)'*(Hnq*V);
             Qp = pinv(Qpinv);
-            
-
-            x = x + (alph*bet)/(alph+bet)* Rinv* Hty(Innov,inth,N,L)+ mvnrnd(pzeros_physical,Qp,L)';
-            Rinv = inv(R + HV*Q*HV');
+            Qp1=Qp*eye(1,N);
+                        % need to solve y = (1/bet)*b for b
+            % calculate Y = (1/bet)*H*V
+            % solve ((1/alph)*eye+(1/bet)*(H*V)^T*H*V)*w =
+            % (1/bet)*(H*V)^T*b for w
+            %             b = y*bet;
+            %             Y = (1/bet)*(Hnq*V);
+            %             w = ((1/alph)+(1/bet)*(Hnq*V)'*(Hnq*V))...
+            %                 /((1/bet)*(Hnq*V)'*b);
+            %             x = (1/bet)*(b-(Hnq*V)*w)
+            %         x = x + Qp*Hnq'*Rinv*Innov + mvnrnd(pzeros_physical,Qp,L)';
+            x = x + (alph*bet)/(alph+bet)* Rinv* Hty(Innov,inth,N,L)+ mvnrnd(pzeros_physical,Qp1,L)';
+            Rinv = inv(R + Hnq*Q*Hnq');
         end
         
         % Reweight
@@ -130,7 +148,7 @@ for i=1:Numsteps
         toSum = exp(toEXP);
         normalizer = logw(1) + log1p(sum(toSum));
         logw = logw - normalizer;
-%         logw = -(1/2)*1\(alph+bet)+logw;
+        logw = -(1/2)*1\(alph+bet)+logw;
         wt = exp(logw);
         
         
@@ -152,19 +170,16 @@ for i=1:Numsteps
     end
     % Compare estimate and truth
     diff_orig= truth(:,i) - (V*estimate(:,i));
-    diff_proj= V*(V'* truth(:,i) - estimate(:,i));
     RMSE_orig = sqrt(diff_orig'*diff_orig/N)
-    RMSE_proj = sqrt(diff_proj'*diff_proj/N)
     MAE_orig = (sum(abs(diff_orig)))/N
     RMSEave_orig = RMSEave_orig + RMSE_orig;
-    RMSEave_proj = RMSEave_proj + RMSE_proj;
     
     % Save to plot
     if mod(i,ObsMult)==0
         %Save RMSE values
         Time(iRMSE)=t;
         RMSEsave(iRMSE)=RMSE_orig;
-        RMSEsave_proj(iRMSE)=RMSE_proj;
+        %         RMSEsave_proj(iRMSE)=RMSE_proj;
         iRMSE = iRMSE+1;
     end
     
@@ -174,15 +189,12 @@ end
 figure
 plot(Time,RMSEsave, 'r-', 'LineWidth', 2)
 grid on
-hold on;
-plot(Time,RMSEsave_proj,'b-','LineWidth', 2)
-%title('The Root Mean-Squared Error')
 xlabel('Time')
 ylabel('RMSE')
 % xticklabels(xticks/dt)
 % ylim([0 0.15])
-legend('RMSE Original','RMSE Projected')
+% legend('RMSE Original','RMSE Projected')
 
 RMSEave_orig = RMSEave_orig/Numsteps
-RMSEave_proj = RMSEave_proj/Numsteps
+% RMSEave_proj = RMSEave_proj/Numsteps
 ResampPercent = ObsMult*Resamps/Numsteps
