d2736b3d8da9c46d327bc01f2ea3b509bb106fed
Marko Budisic <mbudisic@gmail.com>
2020-12-31 01:06:17 -0500

    Reorg of parameters, cleaner storage filename, some commenting

--- Projected_Particle_Filters/Physical_proj_new/PFSWE/SWE_run.m
+++ Projected_Particle_Filters/Physical_proj_new/PFSWE/SWE_run.m
@@ -1,14 +1,14 @@
 close all; clear;clc;
-%Runs
+
+%% SET ALL PARAMETERS HERE
 epsOmega =0.0000001; %For inth = 1
 %Observe every inth variable.
 Numsteps=1440;
-inth=1;%run for 10,100,1000
+inth=1;%observations run for 10,100,1000 
 epsQ=1E-1; %Run for epsQ=1
 epsR=1E-2;
-Num=10;
-Mult=100/Num;
-L=05;
+Num=10; % number of model reduction orders
+L=05; % number of particles
 
 % Observed variables scenario following Paulina et al.
 % scenario 1: observe inth u and v's
@@ -24,29 +24,71 @@ PhysicalProjection =1;
 DataProjection =1 ;
 
 Num_trials=10;
-for j=1:Num
-    for k=1:Num_trials
-    numModes_physical=j*Mult+1;%DMD
-    tolerance_physical=j*Mult;%POD
-    [Time(:,j,k),RMSEsave(:,j,k), RMSEsave_proj(:,j,k),ResampPercent(:,j,k)]=PFSWErun...
-   (numModes_physical,epsQ,epsR,tolerance_physical,iOPPF,PhysicalProjection,DataProjection,scenario,epsOmega, inth,Numsteps,L);
-    end
-end
 
-% % %% Save to mat file
-filename = sprintf('SWEp_%2d%2d_%2d_%2d_%4d_%d_%d.mat',PhysicalProjection,DataProjection,epsQ,epsR,inth,scenario,L)
+Mult=100/Num; % don't touch!
+
+
+%% Parameter storage
+filename = sprintf('SWE_%s_%s_Q%1.2f_R%1.2f_inth%04d_S%d_L%d.mat',...
+    proj2string(PhysicalProjection),...
+    proj2string(DataProjection),...
+    epsQ,...
+    epsR,...
+    inth,...
+    scenario,...
+    L)
+
 params.PhysicalProjection = PhysicalProjection;
 params.DataProjection = DataProjection;
 params.epsQ = epsQ;
 params.epsR=epsR;
 params.iOPPF = iOPPF;
-params.numModes=numModes_physical;
 params.Num=Num;
 params.Mult=Mult;
 params.epsOmega=epsOmega;
+params.Num_trials = Num_trials;
+
+%% Main computation loop
+disp('SWE Main loop started');
+
+for j=1:Num
+    numModes_physical=j*Mult+1;%DMD
+    tolerance_physical=j*Mult;%POD
+    nummodes=numModes_physical-1;
+    params.numModes=numModes_physical; % update to last simulation that was reached for storage purposes
+    
+    for k=1:Num_trials % REPLACE WITH PLAIN FOR IF YOU DON'T WANT PARALLEL EXECUTION
+        [Time(:,j,k),RMSEsave(:,j,k), RMSEsave_proj(:,j,k),ResampPercent(:,j,k)]=PFSWErun...
+            (numModes_physical,epsQ,epsR,tolerance_physical,iOPPF,PhysicalProjection,DataProjection,scenario,epsOmega, inth,Numsteps,L);
+    end
+    disp("SWE CHECKPOINT: Mode choice " +  j + "/" + Num + " - Trials: " + Num_trials + " - TOTAL RUNTIME " + string( duration([0, 0, toc]) )); 
+
+end
+
+%% Results storage
 
 results.RMSEsave = RMSEsave;
 results.RMSEsave_proj = RMSEsave_proj;
 results.ResampPercent =ResampPercent;
 results.Time =Time;
+
+%% Save to drive
 save(filename,'params','results');
+
+%% AUXILIARY FUNCTION
+function out = proj2string(val)
+switch(val)
+    case 0
+        out = 'NON';
+    case 1
+        out = 'POD';
+    case 2
+        out = 'DMD';
+    case 3
+        out = 'AUS';
+    otherwise
+        error("Unknown method")
+end
+end
+
+
