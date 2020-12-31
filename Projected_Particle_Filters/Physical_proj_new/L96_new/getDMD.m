3df7cfd19da38ed67b5f2fb31c781d110de52c38
Marko Budisic <mbudisic@gmail.com>
2020-12-31 00:43:16 -0500

    getDMD now returns real-valued matrices

--- Projected_Particle_Filters/Physical_proj_new/L96_new/getDMD.m
+++ Projected_Particle_Filters/Physical_proj_new/L96_new/getDMD.m
@@ -1,11 +1,28 @@
 function [ q ] = getDMD( Phi,p )
 %Phi is the matrix of DMD modes
-[~,r] = size(Phi);
-if p<=r
-    q = Phi(:,1:p); %this get us p colmuns
-    q=mgs(q);
-else
-    q = Phi; % this is when p>r
-    q=mgs(q);
+q = Phi(:,...
+    1:min(p, size(Phi,2))...
+    ); %this get us p colmuns, or the whole matrix
+
+% DMD modes are typically complex valued. Here we replace conjugate pairs
+% with real/imag parts
+
+for k = 1:size(q,2) - 1
+    
+    % test if two consecutive columns are conjugates
+    if norm( q(:,k) - conj(q(:,k+1)) ) < 1e-6
+        
+        % take cartesian components
+        realpart = real(q(:,k));
+        imagpart = imag(q(:,k));
+        
+        % replace two columns with cartesian components
+        q(:,k) = realpart;
+        q(:,k+1) = imagpart;
+        
+    end
+    
 end
 
+%% modified gram schmidt to get the orthogonal basis
+q = mgs(q);
