55ab5f2240278307de2b524c55d2b23f7560420a
Marko Budisic <mbudisic@gmail.com>
2020-12-31 00:15:49 -0500

    More legible filename

--- Projected_Particle_Filters/Physical_proj_new/L96_new/L96_run_modified.m
+++ Projected_Particle_Filters/Physical_proj_new/L96_new/L96_run_modified.m
@@ -7,6 +7,8 @@ N=40;
 Num=5;
 Mult=N/Num;
 %% Projection_type(0 = no projection, 1 POD, 2 DMD, 3 AUS)
+
+
 PhysicalProjection =2;
 DataProjection =2 ;
 Num_trials=10;
@@ -24,7 +26,14 @@ for j=1:Num
 end
 %% Save to mat file
 PhysicalProjection=2;
-filename = sprintf('L968_%2d%2d_%2d_%2d_%4d.mat',PhysicalProjection,DataProjection,epsQ,epsR,N)
+
+filename = sprintf('L96_F%d_%s_%s_Q%1.2f_R%1.2f_N%04d.mat',...
+    F,...
+    proj2string(PhysicalProjection),...
+    proj2string(DataProjection),...
+    epsQ,...
+    epsR,...
+    N)
 params.PhysicalProjection = PhysicalProjection;
 params.DataProjection = DataProjection;
 params.epsQ = epsQ;
@@ -44,3 +53,19 @@ results.ResampPercent =ResampPercent;
 results.Time =Time;
 
 save(filename,'params','results');
+
+function out = proj2string(val)
+switch(val)
+    case 0
+        out = 'NON';
+    case 1
+        out = 'POD';
+    case 2
+        out = 'DMD';
+    case 3
+        out = 'AUS';
+    otherwise
+        error("Unknown method")
+end
+end
+
