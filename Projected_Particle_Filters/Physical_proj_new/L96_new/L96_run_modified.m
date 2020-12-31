b6363180ade8eb2edda81936e0c4b71354ca6a76
Marko Budisic <mbudisic@gmail.com>
2020-12-31 00:22:13 -0500

    Reorg of the main runner file

--- Projected_Particle_Filters/Physical_proj_new/L96_new/L96_run_modified.m
+++ Projected_Particle_Filters/Physical_proj_new/L96_new/L96_run_modified.m
@@ -1,19 +1,44 @@
 close all; clear;clc;
+
+%% CHOOSE PARAMETERS HERE
 F = 8; % Lorenz'96 Forcing
 ObsMult=5; % % Observe and every ObsMult steps(10 with F=3.5, 5 with F=8)
-epsQ=1;
-epsR=1E-2;
-N=40;
-Num=5;
-Mult=N/Num;
-%% Projection_type(0 = no projection, 1 POD, 2 DMD, 3 AUS)
+epsQ=1; % model covariance
+epsR=1E-2; % observation covariance
 
+N=40; % model size
+Num=5; % number of differend order choices
+Num_trials=10;
 
+% Projection_type(0 = no projection, 1 POD, 2 DMD, 3 AUS)
 PhysicalProjection =2;
 DataProjection =2 ;
-Num_trials=10;
+
+Mult=N/Num; % don't touch this!
+
+
+%% Storage for parameters 
+filename = sprintf('L96_F%d_%s_%s_Q%1.2f_R%1.2f_N%04d.mat',...
+    F,...
+    proj2string(PhysicalProjection),...
+    proj2string(DataProjection),...
+    epsQ,...
+    epsR,...
+    N)
+
+params.PhysicalProjection = PhysicalProjection;
+params.DataProjection = DataProjection;
+params.epsQ = epsQ;
+params.epsR=epsR;
+params.numModes=numModes_physical;
+params.Num=Num;
+params.N=N;
+params.Mult=Mult;
+
+%% Main simulation loop
 for j=1:Num
     for k=1:Num_trials
+        disp("Mode choice " +  j + "/" + Num + " - Trial " + k + "/" + Num_trials); 
         numModes_physical=j*Mult+1;%DMD
         tolerance_physical=j*Mult;%POD
         nummodes=numModes_physical-1;
@@ -24,28 +49,12 @@ for j=1:Num
             (numModes_physical,epsQ, epsR,tolerance_physical,PhysicalProjection,DataProjection,ObsMult,N, F);
     end
 end
-%% Save to mat file
-PhysicalProjection=2;
-
-filename = sprintf('L96_F%d_%s_%s_Q%1.2f_R%1.2f_N%04d.mat',...
-    F,...
-    proj2string(PhysicalProjection),...
-    proj2string(DataProjection),...
-    epsQ,...
-    epsR,...
-    N)
-params.PhysicalProjection = PhysicalProjection;
-params.DataProjection = DataProjection;
-params.epsQ = epsQ;
-params.epsR=epsR;
-params.numModes=numModes_physical;
-params.Num=Num;
-params.N=N;
-params.Mult=Mult;
+%% Storage of results
 
 results.RMSEsave = RMSEsave;
 results.RMSEsave_proj = RMSEsave_proj;
-%% TEMPORARILY NOT SAVING THESE
+%  
+% TEMPORARILY NOT SAVING THESE
 % results.XCsave = XCsave;
 % results.XCsave_proj =XCprojsave;
 % results.ESSsave= ESSsave;
@@ -54,6 +63,7 @@ results.Time =Time;
 
 save(filename,'params','results');
 
+%% AUXILIARY FUNCTION
 function out = proj2string(val)
 switch(val)
     case 0
